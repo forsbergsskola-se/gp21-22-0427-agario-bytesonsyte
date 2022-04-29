100b357f8b7113247ec55397bff70dffe39c4cd6
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-04-29 15:51:26 +0200

    Started work on TcpListener class

--- TimeServer/Program.cs
+++ TimeServer/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TimeServer
+{
+    public class TcpListener
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    
+    
+        System.Net.Sockets.TcpListener TCPListener0 = new System.Net.Sockets.TcpListener(0)
+        {
+            ExclusiveAddressUse = false
+        };
+
+        public TCPClient AcceptTCPClient()
+        {
+            
+            return new TCPClient();
+
+        }
+
+        private void Start()
+        {
+            TCPListener0.Start();
+        }
+
+        public void Stop()
+        {
+            TCPListener0.Stop();
+        
+        }
+    }
+
+    public class TCPClient
+    {
+    
+    }
+
+    public class Stream
+    {
+    
+    } 
+}
+
