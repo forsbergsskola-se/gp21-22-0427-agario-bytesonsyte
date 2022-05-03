4cb95cc6b158d83a6d3adcdc54e07b5b3fc6e74c
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-03 23:43:16 +0200

    Cleaned up most of the program into new methods
    reduced cognitive complexity a ton and can share more between two choices I've introduced so no repeated code

--- TimeServer/Program.cs
+++ TimeServer/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Data;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Net;
 using System.Net.Sockets;
@@ -29,12 +29,13 @@ namespace TimeServer
                 var tcpClient = tcpListener.AcceptTcpClient();
 
                 //IterationOne(tcpListener, tcpClient);
-                StartSimplePokémonGame(tcpListener, tcpClient);
+                StartNewThread(tcpListener, tcpClient);
             }
         }
         
+        
 
-        private static void StartSimplePokémonGame(TcpListener tcpListener, TcpClient tcpClient)
+        private static void StartNewThread(TcpListener tcpListener, TcpClient tcpClient)
         {
             new Thread(() =>
             {
@@ -42,107 +43,7 @@ namespace TimeServer
                 var clientID = tcpClient.Client.RemoteEndPoint;
                 Console.WriteLine($"Cline {clientID} connected"); // client ID print
                 
-                // set up stream and relevant helper classes
-                var stream = tcpClient.GetStream(); // so we can read and write data from the stream
-                var streamReader = new StreamReader(stream); // this negates using encoding.ASCII; easier writing to stream
-                var streamWriter = new StreamWriter(stream);
-                var input = streamReader.ReadLine();
-                streamWriter.AutoFlush = true; // tool to flush stream buffer after every Write(Char) call
-                
-                // TODO: Add Player and AI name options
-                
-                streamWriter.WriteLine("Welcome to the elemental game! Choose either 'Fire', 'Grass' or 'Water'.");
-                streamWriter.WriteLine("If you want to quit though, write 'Exit'");
-                    
-                var playerScore = 0;
-                var aiScore = 0;
-
-                while (true)
-                {
-                    if (input == "Exit")
-                        break;
-                    
-                    var options = new[] {"Water", "Fire", "Grass"};
-                    var random = new Random();
-                    var randomOption = random.Next(0, options.Length);
-                    var aiRandomChoice = options[randomOption];
-                    
-                    switch (input)
-                    {
-                        case "Fire":
-                        case "Grass":
-                        case "Water":
-                            EvaluateMove(input, aiRandomChoice);
-                            break;
-                        
-                        default:
-                            streamWriter.WriteLine("Invalid input. Try again.");
-                            break;
-                    }
-                    
-                    // TODO: try to catch the client if the server crashes 
-                    // TODO: try to catch the client + socket variable if the socket is in use
-                    // TODO: might want to read from the stream asynchronously and/or on a separate thread for realtime scenarios
-                    
-
-                }
-
-                void EvaluateMove(string playerMove, string aiMove)
-                {
-                    streamWriter.Write($"You chose {playerMove}. The AI chose {aiMove}...");
-                    
-                    if (playerMove == aiMove)
-                    {
-                        streamWriter.WriteLine("You were equally matched. Try again."); 
-                        return;
-                    }
-
-                    switch (playerMove)
-                    {
-                        // Fire choice logic
-                        
-                        case "Fire" when aiMove == "Grass":
-                            CalculateScore(playerScore, "Player");
-                            break;
-                        case "Fire":
-                            CalculateScore(aiScore, "AI");
-                            break;
-                        
-                        
-                        // Grass choice logic
-                        
-                        case "Grass" when aiMove == "Water":
-                            CalculateScore(playerScore, "Player");
-
-                            break;
-                        case "Grass":
-                            CalculateScore(aiScore, "AI");
-                            break;
-                        
-                        
-                        // Water choice logic
-                        
-                        case "Water" when aiMove == "Fire":
-                            CalculateScore(playerScore, "Player");
-                            break;
-                        case "Water":
-                            CalculateScore(aiScore, "AI");
-                            break;
-                    }
-                }
-
-                void CalculateScore(int winner, string winnerID)
-                {
-                    if (winner == playerScore)
-                        playerScore++;
-                    else
-                        aiScore++;
-
-                    streamWriter.WriteLine($"The {winnerID} won");
-                    streamWriter.WriteLine($"Scores: {playerScore} - AI: {aiScore}");
-                    
-                }
-                
+                GetStreamGoing(tcpListener, tcpClient);
                 
                 // once while loop has been broken (player client has quit)
                 Console.WriteLine($"Closing the connection to {clientID}");
@@ -150,19 +51,164 @@ namespace TimeServer
                 
             }).Start();
         }
+
+        
+        
+        private static void GetStreamGoing(TcpListener tcpListener, TcpClient tcpClient)
+        {
+            var clientID = tcpClient.Client.RemoteEndPoint;
+            Console.WriteLine($"Cline {clientID} connected"); // client ID print
+                
+            // set up stream and relevant helper classes
+            var stream = tcpClient.GetStream(); // so we can read and write data from the stream
+            var streamReader = new StreamReader(stream); // this negates using encoding.ASCII; easier writing to stream
+            var streamWriter = new StreamWriter(stream);
+            var input = streamReader.ReadLine();
+            streamWriter.AutoFlush = true; // tool to flush stream buffer after every Write(Char) call
+            streamWriter.WriteLine("Input '1' to play Harryokémon or '2' to start the TimeServer ");
+
+            while (true)
+            {
+                if (input == "Exit")
+                    break;
+
+                switch (input)
+                {
+                    case "1":
+                        PlayHarryokémon(streamWriter, input);
+                        break;
+                    case "2":
+                        StartTimeServer(tcpListener, tcpClient);
+                        break;
+                    
+                    default:
+                        InvalidInput(streamWriter);
+                        break;
+                }
+            }
+        }
+        
+        
+        
+        [SuppressMessage("ReSharper", "IdentifierTypo")]
+        private static void PlayHarryokémon(TextWriter streamWriter, string input)
+        {
+            // TODO: Add Player and AI name options
+                
+            streamWriter.WriteLine("Welcome to the elemental game! Choose either 'Fire', 'Grass' or 'Water'.");
+            streamWriter.WriteLine("If you want to quit though, write 'Exit'");
+            
+            var playerScore = 0;
+            var aiScore = 0;
+
+            while (true)
+            {
+                if (input == "Exit")
+                    break;
+                    
+                var options = new[] {"Water", "Fire", "Grass"};
+                var random = new Random();
+                var randomOption = random.Next(0, options.Length);
+                var aiRandomChoice = options[randomOption];
+                    
+                switch (input)
+                {
+                    case "Fire":
+                    case "Grass":
+                    case "Water":
+                        EvaluateMove(streamWriter, input, aiRandomChoice, playerScore, aiScore);
+                        break;
+                        
+                    default:
+                        InvalidInput(streamWriter);
+                        break;
+                }
+                    
+                // TODO: try to catch the client if the server crashes 
+                // TODO: try to catch the client + socket variable if the socket is in use
+                // TODO: might want to read from the stream asynchronously and/or on a separate thread for realtime scenarios
+                    
+            }
+        }
+
+        
+        
+        private static void InvalidInput(TextWriter streamWriter)
+        {
+            streamWriter.WriteLine("Invalid input. Try again.");
+        }
         
 
-        private static void SimpleConnectStreamProgram(TcpListener tcpListener, TcpClient tcpClient)
+        
+        private static void EvaluateMove(TextWriter streamWriter, string playerMove, string aiMove, int playerScore,
+            int aiScore)
         {
+            streamWriter.Write($"You chose {playerMove}. The AI chose {aiMove}...");
+
+            if (playerMove == aiMove)
+            {
+                streamWriter.WriteLine("You were equally matched. Try again.");
+                return;
+            }
+
+            switch (playerMove)
+            {
+                // Fire choice logic
+
+                case "Fire" when aiMove == "Grass":
+                    CalculateScore(streamWriter, playerScore, "Player", aiScore);
+                    break;
+                case "Fire":
+                    CalculateScore(streamWriter, aiScore, "AI", playerScore);
+                    break;
+
+
+                // Grass choice logic
+
+                case "Grass" when aiMove == "Water":
+                    CalculateScore(streamWriter, playerScore, "Player", aiScore);
+
+                    break;
+                case "Grass":
+                    CalculateScore(streamWriter, aiScore, "AI", playerScore);
+                    break;
+
+
+                // Water choice logic
+
+                case "Water" when aiMove == "Fire":
+                    CalculateScore(streamWriter, playerScore, "Player", aiScore);
+                    break;
+                case "Water":
+                    CalculateScore(streamWriter, aiScore, "AI", playerScore);
+                    break;
+            }
+        }
+        
+        
+        
+        private static void CalculateScore(TextWriter streamWriter, int winnerScore, string winnerID, int loserScore)
+        {
+            winnerScore++;
+
+            streamWriter.WriteLine($"The {winnerID} won");
+            streamWriter.WriteLine($"Scores: {winnerScore} - AI: {loserScore}");
+        }
+
+
+        private static void StartTimeServer(TcpListener tcpListener, TcpClient tcpClient)
+        {
+            Console.WriteLine($"Client {tcpClient.Client.RemoteEndPoint} connected!");
             var bufferSize = new byte[100];
             tcpClient.GetStream().Read(bufferSize, 0, 100);
-                
+
             var comment = Encoding.ASCII.GetString(bufferSize);
             Console.WriteLine($"New comment from Client: {comment}");
-                
+
             var currentTime = Encoding.ASCII.GetBytes(DateTime.Now.ToString(CultureInfo.CurrentCulture));
             tcpClient.GetStream().Write(currentTime, 0, currentTime.Length);
             tcpClient.Close();
+            tcpListener.Stop();
         }
     }
 }
