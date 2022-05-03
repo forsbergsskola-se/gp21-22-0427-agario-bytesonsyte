67ec86866d5dfc710eea369de290608bbf4fe061
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-03 16:52:20 +0200

    Game rules set-up

--- TimeServer/Program.cs
+++ TimeServer/Program.cs
@@ -28,12 +28,12 @@ namespace TimeServer
                 var tcpClient = tcpListener.AcceptTcpClient();
 
                 //IterationOne(tcpListener, tcpClient);
-                IterationTwo(tcpListener, tcpClient);
+                SimplePokémonGame(tcpListener, tcpClient);
             }
         }
         
 
-        private static void IterationTwo(TcpListener tcpListener, TcpClient tcpClient)
+        private static void SimplePokémonGame(TcpListener tcpListener, TcpClient tcpClient)
         {
             new Thread(() =>
             {
@@ -44,13 +44,25 @@ namespace TimeServer
                 var streamReader = new StreamReader(stream); // this negates using encoding.ASCII; easier writing to stream
                 var streamWriter = new StreamWriter(stream);
 
-                streamWriter.AutoFlush = true; // tool to flush stream buffer after every Write(Char) call 
+                streamWriter.AutoFlush = true; // tool to flush stream buffer after every Write(Char) call
+                
+                var options = new[] {"Water, Fire, Grass"};
+                streamWriter.WriteLine("Welcome to the elemental game! Choose either 'Fire', 'Grass' or 'Water'.");
+                streamWriter.WriteLine("If you want to quit though, write 'Exit'");
+                var random = new Random();
+                int playerScore = 0;
+                int aiScore = 0;
 
+                while (true)
+                {
+                    
+                }
+                
             }).Start();
         }
         
         
-        private static void IterationOne(TcpListener tcpListener, TcpClient tcpClient)
+        private static void SimpleConnectStreamProgram(TcpListener tcpListener, TcpClient tcpClient)
         {
             var bufferSize = new byte[100];
             tcpClient.GetStream().Read(bufferSize, 0, 100);
