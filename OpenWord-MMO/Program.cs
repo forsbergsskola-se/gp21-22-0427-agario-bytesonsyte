20fdfa75556539e0b72c770b847c73e3f9e5f6f3
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-08 01:34:40 +0200

    Setting up main project script for OpenWord-MMO assignment

--- OpenWord-MMO/Program.cs
+++ OpenWord-MMO/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace OpenWord_MMO;
+
+internal static class OpenGameServer
+{
+    private static void Main()
+    {
+        var endpoint = new IPEndPoint(IPAddress.Loopback, 3333);
+        var server = new UdpClient(endpoint);
+        
+        Console.WriteLine("The server is starting...");
+    }
+}
