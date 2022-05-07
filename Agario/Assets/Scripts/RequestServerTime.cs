baee416ca6e5f3f1da4e7f50145aef241c88e8ba
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-07 23:42:37 +0200

    added TcpClient

--- Agario/Assets/Scripts/RequestServerTime.cs
+++ Agario/Assets/Scripts/RequestServerTime.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class RequestServerTime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    readonly IPEndPoint Endpoint = new (IPAddress.Loopback, 4444); // originally used loopback
+    public void SendRequest()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var tcpClient = new TcpClient(Endpoint);
+        var stream = tcpClient.GetStream();
     }
 }
