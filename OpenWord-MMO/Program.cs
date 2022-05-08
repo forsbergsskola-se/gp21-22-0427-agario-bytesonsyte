747e9a35051aac669d23a9c34d337357f147966c
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-08 23:46:35 +0200

    server can flag message if its over 20 characters or has a whitespace

--- OpenWord-MMO/Program.cs
+++ OpenWord-MMO/Program.cs
@@ -1,23 +1,34 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace OpenWord_MMO;
 
 internal static class OpenGameServer
 {
+    private const byte MaxMessageCharSize = 20;
+
     private static void Main()
     {
         Console.WriteLine("Server initializing");
-        var endpoint = new IPEndPoint(IPAddress.Loopback, 3333);
-        var udpClient = new UdpClient(endpoint);
+        var remoteEP = new IPEndPoint(IPAddress.Loopback, 3333);
+        var udpClient = new UdpClient(remoteEP);
 
-        Console.WriteLine($"Server set up at port {endpoint.Port.ToString()}");
+        Console.WriteLine($"Server set up at port {remoteEP.Port.ToString()}");
         Console.WriteLine("Server ready & waiting to receive");
 
         while (true)
         {
             IPEndPoint? remoteEndpoint = default;
-            var udpData = udpClient.Receive(ref remoteEndpoint);
+            var data = udpClient.Receive(ref remoteEndpoint);
+            var messageString = Encoding.ASCII.GetString(data).Trim();
+            Console.WriteLine($"Number of characters of received message: {data.Length}");
+
+            if (data.Length > MaxMessageCharSize || messageString.Any(char.IsWhiteSpace))
+            {
+                Console.WriteLine("Error: message is over 20 characters or contained a whitespace");
+                continue;
+            }
         }
     }
 }
