8c27627dea63ca8197899848f19344b80cf9938e
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-08 23:22:33 +0200

    figured out how to get udpclient to receive

--- OpenWord-MMO/Program.cs
+++ OpenWord-MMO/Program.cs
@@ -7,9 +7,17 @@ internal static class OpenGameServer
 {
     private static void Main()
     {
+        Console.WriteLine("Server initializing");
         var endpoint = new IPEndPoint(IPAddress.Loopback, 3333);
-        var server = new UdpClient(endpoint);
-        
-        Console.WriteLine("The server is starting...");
+        var udpClient = new UdpClient(endpoint);
+
+        Console.WriteLine($"Server set up at port {endpoint.Port.ToString()}");
+        Console.WriteLine("Server ready & waiting to receive");
+
+        while (true)
+        {
+            IPEndPoint? remoteEndpoint = default;
+            var udpData = udpClient.Receive(ref remoteEndpoint);
+        }
     }
 }
