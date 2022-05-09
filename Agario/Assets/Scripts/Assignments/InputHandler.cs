43001fb8cd5d46931c21d4c28a738277f9eaa6fc
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-09 04:19:28 +0200

    added server networking back now that everything is working

--- Agario/Assets/Scripts/Assignments/InputHandler.cs
+++ Agario/Assets/Scripts/Assignments/InputHandler.cs
@@ -13,7 +13,7 @@ namespace Assignments
         private TMP_Text OutputField;
         private TMP_InputField InputField;
         [SerializeField] private String SavedText;
-        [SerializeField] private const int MaxCharSize = 20;
+        private const int MaxCharSize = 20;
         private void Awake()
         {
             OutputField = GameObject.FindGameObjectWithTag("Output").GetComponent<TMP_Text>();
@@ -23,21 +23,26 @@ namespace Assignments
 
         public void OutputText()
         {
-            //var remoteEndpoint = new IPEndPoint(IPAddress.Loopback, 3333);
-            //var udpClient = new UdpClient(remoteEndpoint);
-            //udpClient.Connect(remoteEndpoint);
+            var remoteEndpoint = new IPEndPoint(IPAddress.Loopback, 3333);
+            var udpClient = new UdpClient(remoteEndpoint);
+            udpClient.Connect(remoteEndpoint);
 
             var input = InputField.text;
-            //var inputBytes = Encoding.ASCII.GetBytes(InputField.text);
-            //udpClient.Send(input, input.Length);
+            var inputBytes = Encoding.ASCII.GetBytes(InputField.text);
 
             if (input.Length is > MaxCharSize or 0 || input.Any(char.IsWhiteSpace))
+            {
                 InputField.text = "Error";
+                var errorMessage = Encoding.ASCII.GetBytes("Incorrect formatting. Must be under 20 characters and have no whitespaces");
+                udpClient.Send(errorMessage, errorMessage.Length);
+            }
             else
             {
                 SavedText = SavedText + input + " ";
                 OutputField.text = SavedText;
                 InputField.text = ""; // clear field
+                
+                udpClient.Send(inputBytes, input.Length);
             }
         }
     }
