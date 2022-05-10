dd7ec38158bb9e7cbaa5bacaf5dec9bdaba21fb8
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-10 23:57:19 +0200

    Update Consume.cs: successfully detecting food

--- Agario/Assets/Scripts/Agario/Player/Consume.cs
+++ Agario/Assets/Scripts/Agario/Player/Consume.cs
@@ -4,6 +4,11 @@ namespace Agario.Player
 {
     public class Consume : MonoBehaviour
     {
-        
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Food"))
+                Debug.Log($"{gameObject.name} ate {other.gameObject.name}");
+
+        }
     }
 }
