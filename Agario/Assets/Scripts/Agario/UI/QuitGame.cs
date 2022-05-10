1c41574a7d6cbef0e8a81d718035e312e3656944
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-10 22:26:32 +0200

    Created new script - QuitGame.cs

---
+++ Agario/Assets/Scripts/Agario/UI/QuitGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Agario.UI
+{
+    public class QuitGame : MonoBehaviour
+    {
+        public void QuitApplication()
+        {
+            Application.Quit();
+        }
+    }
+}
