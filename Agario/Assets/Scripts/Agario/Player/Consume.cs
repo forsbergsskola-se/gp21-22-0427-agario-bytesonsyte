f105022b15b76479389ede44e37009d4f5e62b2b
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-11 01:17:40 +0200

    cleaned up Consume.cs into several methods

--- Agario/Assets/Scripts/Agario/Player/Consume.cs
+++ Agario/Assets/Scripts/Agario/Player/Consume.cs
@@ -1,7 +1,6 @@
 using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Android;
 
 namespace Agario.Player
 {
@@ -18,30 +17,48 @@ namespace Agario.Player
         {
             var food = other.gameObject.CompareTag("Food");
             var enemy = other.gameObject.CompareTag("Player");
-            
+
             if (food)
             {
-                Debug.Log($"{PlayerName} ate {other.gameObject.name}");
-                Destroy(other.gameObject);
+                ConsumeFood(other);
+                //TODO: increase score
+                //TODO: inscrease size
                 return;
             }
-
-            if (!enemy) return;
             
+            if (!enemy) return;
+            CalculateEnemyOutcome(other);
+        }
+
+        
+        
+        private void ConsumeFood(Collision2D other)
+        {
+            Debug.Log($"{PlayerName} ate {other.gameObject.name}");
+            Destroy(other.gameObject);
+        }
+
+        
+        
+        private void CalculateEnemyOutcome(Collision2D other)
+        {
             var playerScale = transform.localScale.x;
             var enemyScale = other.gameObject.transform.localScale.x;
             var enemyName = other.gameObject.GetComponentInChildren<TMP_Text>().text;
 
+            // Player wins, eats Enemy
             if (playerScale > enemyScale)
             {
                 Debug.Log($"{gameObject.name} ate {enemyName}");
-                //TODO: also consume enemy score
                 Destroy(other.gameObject);
+                //TODO: also consume enemy score
             }
-                
-            else if (Math.Abs(playerScale - enemyScale) < ScaleTolerance)
+            
+            // It's a draw, both go free
+            else if (Math.Abs(playerScale - enemyScale) < ScaleTolerance) // has a tolerance check to make up for inaccurate float comparisons
                 Debug.Log($"{PlayerName} can't eat {enemyName} [Scale sizes are equal]");
 
+            //Player loses, print impending death
             else
                 Debug.Log($"{PlayerName} is being consumed by {enemyName} [Scale size higher: {enemyScale}:{playerScale}]");
         }
