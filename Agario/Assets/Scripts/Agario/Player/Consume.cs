133fc226464bfdd14ce6f2eafea0a10c3026fa80
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-11 01:01:20 +0200

    Update Consume.cs: Set-up logic for enemy collision

--- Agario/Assets/Scripts/Agario/Player/Consume.cs
+++ Agario/Assets/Scripts/Agario/Player/Consume.cs
@@ -1,10 +1,13 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Android;
 
 namespace Agario.Player
 {
     public class Consume : MonoBehaviour
     {
+        public float ScaleTolerance = 0.01f;
         private string PlayerName;
         private void Start()
         {
@@ -13,6 +16,9 @@ namespace Agario.Player
 
         private void OnCollisionEnter2D(Collision2D other)
         {
+            var playerScale = transform.localScale.x;
+            var enemyScale = other.gameObject.transform.localScale.x;
+            
             var food = other.gameObject.CompareTag("Food");
             var enemy = other.gameObject.CompareTag("Player");
             
@@ -20,6 +26,24 @@ namespace Agario.Player
             {
                 Debug.Log($"{PlayerName} ate {other.gameObject.name}");
                 Destroy(other.gameObject);
+            }
+            
+            else if (enemy)
+            {
+                var enemyName = other.gameObject.GetComponentInChildren<TMP_Text>().text;
+
+                if (playerScale > enemyScale)
+                {
+                    Debug.Log($"{gameObject.name} ate {enemyName}");
+                    //TODO: also consume enemy score
+                    Destroy(other.gameObject);
+                }
+                
+                else if (Math.Abs(playerScale - enemyScale) < ScaleTolerance)
+                    Debug.Log($"{PlayerName} can't eat {enemyName} [Scale sizes are equal]");
+
+                else
+                    Debug.Log($"{PlayerName} is being consumed by {enemyName} [Scale size higher: {enemyScale}:{playerScale}]");
             }
         }
     }
