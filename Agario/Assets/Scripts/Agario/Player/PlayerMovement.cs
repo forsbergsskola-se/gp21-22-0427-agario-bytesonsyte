2c230c5b39ce9f485c3eee5e4191bb2ba1521d50
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-10 23:20:49 +0200

    Basic player movement is working - using mouse location

--- Agario/Assets/Scripts/Agario/Player/PlayerMovement.cs
+++ Agario/Assets/Scripts/Agario/Player/PlayerMovement.cs
@@ -1,9 +1,31 @@
+using System;
 using UnityEngine;
 
 namespace Agario.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-       
+        [SerializeField] public float Speed;
+        private Vector3 TargetPos;
+        private Vector3 CurrentPos;
+        private Camera mainCam;
+
+        private void Start()
+        {
+            mainCam = Camera.main;
+        }
+
+        public void FixedUpdate()
+        {
+            CurrentPos = transform.position;
+            TargetPos = mainCam!.ScreenToWorldPoint(Input.mousePosition); // gets targetPos from mouse location (with a null check)
+        }
+
+        private void Update()
+        {
+            TargetPos.z = CurrentPos.z;
+            transform.position = Vector3.MoveTowards(CurrentPos, TargetPos, // constantly move towards mouse (TargetPos)
+                Speed * Time.deltaTime);  // use deltaTime to negate computer performance effecting PlayerSpeed
+        }
     }
 }
