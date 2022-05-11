7112d9f7907f9fb7f2d4b135ac8842ce9f6fcbf5
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-11 15:49:20 +0200

    Added new script: PlayerScore.cs

---
+++ Agario/Assets/Scripts/Agario/Player/PlayerScore.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Agario.Player
+{
+    public class PlayerScore : MonoBehaviour
+    {
+        public int Score;
+        private TMP_Text ScoreUI;
+        
+        private void Start()
+        {
+            Score = 0;
+            ScoreUI = FindObjectOfType<TextMeshProUGUI>().GetComponent<TMP_Text>();
+        }
+
+        public void IncreaseScore(int enemyScore)
+        {
+            Score += enemyScore;
+            ScoreUI.text = "Score: " + Score;
+        }
+    }
+}
