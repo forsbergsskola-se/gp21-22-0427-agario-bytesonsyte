67185b45d0f307267f1dbb986553e7db3960f9f0
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-14 15:12:43 +0200

    playerMovement is now independent of frameRate

--- Agario/Assets/Scripts/Agario/Player/PlayerMovement.cs
+++ Agario/Assets/Scripts/Agario/Player/PlayerMovement.cs
@@ -14,16 +14,21 @@ namespace Agario.Player
             mainCam = Camera.main;
         }
 
+        private void FixedUpdate()
+        {
+            TargetPos = mainCam!.ScreenToWorldPoint(Input
+                .mousePosition); // gets targetPos from mouse location (with a null check)
+        }
+
 
         private void Update()
         {
             CurrentPos = transform.position;
-            TargetPos = mainCam!.ScreenToWorldPoint(Input
-                .mousePosition); // gets targetPos from mouse location (with a null check)
-        
             TargetPos.z = CurrentPos.z;
-            transform.position = Vector3.MoveTowards(CurrentPos, TargetPos, // constantly move towards mouse (TargetPos)
-                (float) Speed * Time.deltaTime); // use deltaTime to negate computer performance effecting PlayerSpeed
+            
+            var scale = 1.0f - (float)System.Math.Pow(0.95, Time.deltaTime * 60.0f); // makes deltaTime independent of framerate
+            gameObject.transform.position = Vector3.MoveTowards(CurrentPos, TargetPos, // constantly move towards mouse (TargetPos)
+                (float) Speed * scale); // use deltaTime to negate computer performance effecting PlayerSpeed
             // transform.localScale.x);  // the higher the player's scale, the lower their speed
         }
     }
