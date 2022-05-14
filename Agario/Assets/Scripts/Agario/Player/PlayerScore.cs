1817c14d260044371938de11aaef4768b38d4c9f
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-05-14 19:48:50 +0200

    added highScore logic

--- Agario/Assets/Scripts/Agario/Player/PlayerScore.cs
+++ Agario/Assets/Scripts/Agario/Player/PlayerScore.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 
 namespace Agario.Player
@@ -6,18 +7,24 @@ namespace Agario.Player
     public class PlayerScore : MonoBehaviour
     {
         public int Score;
-        private TMP_Text ScoreUI;
+        public TMP_Text ScoreUI;
+        public TMP_Text HighScoreUI;
+        public int HighScore;
         
         private void Start()
         {
             Score = 0;
-            ScoreUI = FindObjectOfType<TextMeshProUGUI>().GetComponent<TMP_Text>();
+            HighScore = Convert.ToInt32(HighScoreUI.text); // 0 at launch, but updated otherwise
         }
 
         public void IncreaseScore(int enemyScore)
         {
             Score += enemyScore;
             ScoreUI.text = "Score: " + Score;
+
+            if (Score !> HighScore) return;
+            HighScore = Score;
+            HighScoreUI.text = HighScore.ToString();
         }
     }
 }
